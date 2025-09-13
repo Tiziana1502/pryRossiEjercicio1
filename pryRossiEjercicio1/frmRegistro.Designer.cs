6da7a5f4db4b44c3fec32105bb9957892a25a2df
JuanGiovannetti <giovannettijuan123@gmail.com>
2025-09-12 21:12:00 -0300

    codigo finalizado

--- pryRossiEjercicio1/frmRegistro.Designer.cs
+++ pryRossiEjercicio1/frmRegistro.Designer.cs
@@ -41,9 +41,10 @@
             // lblUsuario
             // 
             lblUsuario.AutoSize = true;
-            lblUsuario.Location = new Point(291, 74);
+            lblUsuario.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold);
+            lblUsuario.Location = new Point(41, 49);
             lblUsuario.Name = "lblUsuario";
-            lblUsuario.Size = new Size(47, 15);
+            lblUsuario.Size = new Size(68, 23);
             lblUsuario.TabIndex = 0;
             lblUsuario.Text = "Usuario";
             // 
@@ -53,76 +54,89 @@
             cmbModulo.Enabled = false;
             cmbModulo.FormattingEnabled = true;
             cmbModulo.Items.AddRange(new object[] { "ADM", "SIST", "COM", "VTA" });
-            cmbModulo.Location = new Point(376, 174);
+            cmbModulo.Location = new Point(144, 181);
+            cmbModulo.Margin = new Padding(3, 4, 3, 4);
             cmbModulo.Name = "cmbModulo";
-            cmbModulo.Size = new Size(135, 23);
+            cmbModulo.Size = new Size(154, 28);
             cmbModulo.TabIndex = 1;
             cmbModulo.SelectedIndexChanged += cmbModulo_SelectedIndexChanged;
             // 
             // txtUsuario
             // 
-            txtUsuario.Location = new Point(376, 74);
+            txtUsuario.Location = new Point(144, 48);
+            txtUsuario.Margin = new Padding(3, 4, 3, 4);
             txtUsuario.MaxLength = 10;
             txtUsuario.Name = "txtUsuario";
-            txtUsuario.Size = new Size(135, 23);
+            txtUsuario.Size = new Size(154, 27);
             txtUsuario.TabIndex = 3;
             txtUsuario.TextChanged += txtUsuario_TextChanged;
             // 
             // lblContraseña
             // 
             lblContraseña.AutoSize = true;
-            lblContraseña.Location = new Point(291, 126);
+            lblContraseña.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold);
+            lblContraseña.Location = new Point(41, 118);
             lblContraseña.Name = "lblContraseña";
-            lblContraseña.Size = new Size(67, 15);
+            lblContraseña.Size = new Size(97, 23);
             lblContraseña.TabIndex = 4;
             lblContraseña.Text = "Contraseña";
             // 
             // lblModulo
             // 
             lblModulo.AutoSize = true;
-            lblModulo.Location = new Point(291, 177);
+            lblModulo.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold);
+            lblModulo.Location = new Point(41, 186);
             lblModulo.Name = "lblModulo";
-            lblModulo.Size = new Size(49, 15);
+            lblModulo.Size = new Size(70, 23);
             lblModulo.TabIndex = 5;
             lblModulo.Text = "Módulo";
             // 
             // txtContraseña
             // 
             txtContraseña.Enabled = false;
-            txtContraseña.Location = new Point(376, 126);
+            txtContraseña.Location = new Point(144, 118);
+            txtContraseña.Margin = new Padding(3, 4, 3, 4);
             txtContraseña.MaxLength = 10;
             txtContraseña.Name = "txtContraseña";
             txtContraseña.PasswordChar = '*';
-            txtContraseña.Size = new Size(135, 23);
+            txtContraseña.Size = new Size(154, 27);
             txtContraseña.TabIndex = 6;
             txtContraseña.TextChanged += txtContraseña_TextChanged;
             // 
             // btnCancelar
             // 
-            btnCancelar.Location = new Point(543, 126);
+            btnCancelar.BackColor = Color.SteelBlue;
+            btnCancelar.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            btnCancelar.ForeColor = SystemColors.ControlLightLight;
+            btnCancelar.Location = new Point(179, 243);
+            btnCancelar.Margin = new Padding(3, 4, 3, 4);
             btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(99, 23);
+            btnCancelar.Size = new Size(105, 34);
             btnCancelar.TabIndex = 7;
             btnCancelar.Text = "Cancelar";
-            btnCancelar.UseVisualStyleBackColor = true;
+            btnCancelar.UseVisualStyleBackColor = false;
             btnCancelar.Click += btnCancelar_Click;
             // 
             // btnAceptar
             // 
+            btnAceptar.BackColor = Color.SteelBlue;
             btnAceptar.Enabled = false;
-            btnAceptar.Location = new Point(543, 73);
+            btnAceptar.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            btnAceptar.ForeColor = SystemColors.ControlLightLight;
+            btnAceptar.Location = new Point(41, 243);
+            btnAceptar.Margin = new Padding(3, 4, 3, 4);
             btnAceptar.Name = "btnAceptar";
-            btnAceptar.Size = new Size(99, 23);
+            btnAceptar.Size = new Size(105, 34);
             btnAceptar.TabIndex = 8;
             btnAceptar.Text = "Aceptar";
-            btnAceptar.UseVisualStyleBackColor = true;
+            btnAceptar.UseVisualStyleBackColor = false;
             btnAceptar.Click += btnAceptar_Click;
             // 
             // frmRegistro
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(717, 327);
+            ClientSize = new Size(354, 338);
             Controls.Add(btnAceptar);
             Controls.Add(btnCancelar);
             Controls.Add(txtContraseña);
@@ -131,6 +145,7 @@
             Controls.Add(txtUsuario);
             Controls.Add(cmbModulo);
             Controls.Add(lblUsuario);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "frmRegistro";
             Text = "Login";
             Load += frmRegistro_Load;
--- pryRossiEjercicio1/frmRegistro.cs
+++ pryRossiEjercicio1/frmRegistro.cs
@@ -16,6 +16,8 @@ namespace pryRossiEjercicio1
         {
             InitializeComponent();
         }
+        private int Intentos = 0;
+        //Contador pra validar la cantidad de intentos del usuario
 
         private void frmRegistro_Load(object sender, EventArgs e)
         {
@@ -24,6 +26,47 @@ namespace pryRossiEjercicio1
 
         private void btnAceptar_Click(object sender, EventArgs e)
         {
+            {
+                //El boton te lleva de un formulario a otro, primero validando que los datos son correctos
+                // && operador logico AND
+                // || operador logico OR 
+
+                if (txtUsuario.Text == "Adm" && txtContraseña.Text == "1@" && (cmbModulo.SelectedIndex == 0 || cmbModulo.SelectedIndex == 2 || cmbModulo.SelectedIndex == 3))
+                {
+                    Intentos = 0;
+                    //para crear una ventana del formulario que se indica
+                    // Modal significa que si no se cierra la otra ventana no se puede continuar
+                    frmInicio frminicio = new frmInicio();
+                    frminicio.ShowDialog();
+                }
+                else if (txtUsuario.Text == "John" && txtContraseña.Text == "*2b" && cmbModulo.SelectedIndex == 1)
+                {
+                    Intentos = 0;
+                    frmInicio frminicio = new frmInicio();
+                    frminicio.ShowDialog();
+                }
+                else if (txtUsuario.Text == "Ceci" && txtContraseña.Text == "*@3c" && (cmbModulo.SelectedIndex == 0 || cmbModulo.SelectedIndex == 3))
+                {
+                    Intentos = 0;
+                    frmInicio frminicio = new frmInicio();
+                    frminicio.ShowDialog();
+                }
+                else if (txtUsuario.Text == "God" && txtContraseña.Text == "*@#4d" && (cmbModulo.SelectedIndex == 0 || cmbModulo.SelectedIndex == 1 || cmbModulo.SelectedIndex == 2 || cmbModulo.SelectedIndex == 3))
+                {
+                    Intentos = 0;
+                    frmInicio frminicio = new frmInicio();
+                    frminicio.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Usuario y/o contraseña incorrectos para el módulo seleccionado");
+                    Intentos++;
+                    if (Intentos == 3)
+                    {
+                        this.Close(); //cierra el formulario
+                    }
+                }
+            }
             //para crear una ventana del formulario que se indica
             // Modal significa que si no se cierra la otra ventana no se puede continuar
             frmInicio frmInicio = new frmInicio();
